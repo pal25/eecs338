62d3e985e7db7f9b0438e31b810ef761f64ae6fa
Patrick Landis <pal25@case.edu>
2013-02-11 17:06:53 -0500

    Updated code and added (limited) debugging

--- hw2/sort.c
+++ hw2/sort.c
@@ -14,8 +14,10 @@
 #define STDIN_FD 0
 #define STDOUT_FD 1
 #define STDERR_FD 2
+#define WRITE_END 1
+#define READ_END 0
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 256
 
 char* getbytes(int fd)
 {
@@ -23,9 +25,8 @@ char* getbytes(int fd)
   FILE *stream = fdopen(fd, "r");
  
   size_t contentSize = 1;
-  char *content = malloc(sizeof(char)*BUFFER_SIZE);
-  content[0] = '\0';
-
+  char *content = calloc(BUFFER_SIZE, sizeof(char));
+ 
   if(content == NULL) {
     perror("Failed to allocate content");
     exit(ALLOC_ERROR);
@@ -67,64 +68,109 @@ pid_t checked_fork()
   return pid;
 }
 
-void split(int *inpipe, int *outpipe)
+void split_workers(int *inpipe, int *outpipe)
 {
   checked_pipe(inpipe);
   checked_pipe(outpipe);
   pid_t child = checked_fork();
   if(child == 0) {
-    printf("CHILD: %d\n", getpid());
-
-    dup2(inpipe[0], STDIN_FD);   // Make sure the new proc can read
-    dup2(outpipe[1], STDOUT_FD); // Make sure the new proc can write
-    dup2(outpipe[1], STDERR_FD); // Just in case make sure new proc can err
+    dup2(inpipe[READ_END], STDIN_FD);   // Make sure the new proc can read
+    dup2(outpipe[WRITE_END], STDOUT_FD); // Make sure the new proc can write
+    dup2(outpipe[WRITE_END], STDERR_FD); // Just in case make sure new proc can err
     
-    close(inpipe[1]);  // Proc wont write to the inpipe
-    close(outpipe[0]); // Proc wont read from the outpipe
+    close(inpipe[WRITE_END]);  // Proc wont write to the inpipe
+    close(outpipe[READ_END]); // Proc wont read from the outpipe
 
     execl("./sort", "", (char*) NULL); 
     exit(EXEC_ERROR);
   } else {
-    close(inpipe[0]);  // The parent wont read from inpipe
-    close(outpipe[1]); // Parent wont write from outpipe
+    close(inpipe[READ_END]);  // The parent wont read from inpipe
+    close(outpipe[WRITE_END]); // Parent wont write from outpipe
   }
+}
+
+void merge(char *out, char *m1, char *m2)
+{
+  int m1_len = strlen(m1);
+  int m2_len = strlen(m2);
+  
+  int m1_ptr = 0;
+  int m2_ptr = 0;
+  int out_ptr = 0;
+
+  while(out_ptr < m1_len + m2_len) {
+    //printf("Out_ptr %d, m1_len %d, m2_len %d, m1[%d]:%c, m2[%d]:%c\n", out_ptr, m1_len, m2_len, m1_ptr, m1[m1_ptr], m2_ptr, m2[m2_ptr]);
+    if(m1_ptr >= strlen(m1)) {
+      out[out_ptr] = m2[m2_ptr];
+      m2_ptr++;
+    } else if (m2_ptr >= strlen(m2)) {
+      out[out_ptr] = m1[m1_ptr];
+      m1_ptr++;
+    } else if(m1[m1_ptr] < m2[m2_ptr]) {
+      out[out_ptr] = m1[m1_ptr];
+      m1_ptr++;
+    } else {
+      out[out_ptr] = m2[m2_ptr];
+      m2_ptr++;
+    }
+    out_ptr++;
+  }
+
+  out[out_ptr] = '\0';
+
+  FILE* dbg_fd = fopen("./debug", "a");
+  fprintf(dbg_fd, "Content(PID=%d): Left=%s, Right=%s, Merged=%s\n", getpid(), m1, m2, out);
+  fclose(dbg_fd);
+
+}
+
+void split_array(char* content, char* left, char* right)
+{
+  int len = strlen(content);
+
+  strncpy(left, content, len/2);
+  left[len/2] = '\0';
+  strncpy(right, content + len/2, len);
+   
 }
 
 int main(int argc, char** argv)
 {
+  
+  //char out[BUFFER_SIZE];
+  //merge(out, "aceg", "bdfh");
+  //printf("Merge: %s\n", out);
+  
   char *content = getbytes(STDIN_FD);
-  printf("Content: %s\n", content);
   int len = strlen(content);
  
   if(len <= 1) {
-    fprintf(stdin, "%s", content);
+    FILE* dbg_fd = fopen("./debug", "a");
+    fprintf(dbg_fd, "Content(PID=%d): %s\n", getpid(), content);
+    fclose(dbg_fd);
+    fprintf(stdout, "%s", content); //Send char back up the pipe
   } else {
     char left[BUFFER_SIZE], right[BUFFER_SIZE];
-    strncpy(left, content, len/2);
-    left[len/2] = '\0';
-    strncpy(right, content + len/2, (len - len/2));
-    printf("LEFT(LEN=%d): %s\n", strlen(left), left);
-    //printf("RIGHT(LEN=%d): %s\n", strlen(right), right);
-
-    int linpipe[2], loutpipe[2];
-    split(linpipe, loutpipe);
-    write(linpipe[1], left, strlen(left));
-    close(linpipe[1]);
+    split_array(content, left, right);
    
-    //int rinpipe[2], routpipe[2];
-    //split(rinpipe, routpipe);
-    //write(rinpipe[1], right, strlen(right));
-    //close(rinpipe[1]);
+    int linpipe[2], loutpipe[2];
+    split_workers(linpipe, loutpipe); // Create a new proc to send to
+    write(linpipe[WRITE_END], left, strlen(left));
+    close(linpipe[WRITE_END]);
+   
+    int rinpipe[2], routpipe[2];
+    split_workers(rinpipe, routpipe); // Create a new proc to send to
+    write(rinpipe[WRITE_END], right, strlen(right));
+    close(rinpipe[WRITE_END]);
     
-    char* left_content = getbytes(loutpipe[0]);
-    close(loutpipe[0]);
+    char* left_content = getbytes(loutpipe[READ_END]);
+    close(loutpipe[READ_END]);
 
-    //char* right_content = getbytes(routpipe[0]);
-    // close(loutpipe[0]);
-    
+    char* right_content = getbytes(routpipe[READ_END]);
+    close(routpipe[READ_END]);
+
     char output[BUFFER_SIZE];
-    strcat(output, left_content);
-    //strcat(output, right_content);
+    merge(output, left_content, right_content);
     write(STDOUT_FD, content, strlen(content));
   }
 
