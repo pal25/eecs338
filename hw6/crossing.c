b35ca81e928c28a43f164ccbd7a44739f97a2f7f
Patrick Landis <pal25@case.edu>
2013-03-22 22:14:10 -0400

    Switched L to A and R to B for my sanity

--- hw6/crossing.c
+++ hw6/crossing.c
@@ -5,24 +5,21 @@
 #include <sys/time.h>
 #include <time.h>
 
-
-#define LEFT_NUM 1
-#define RIGHT_NUM 3
+#define A_NUM 1
+#define B_NUM 3
 
 #define THREAD_SHARED 0
-#define RAND_SEED 5135
-
 
 // Global Variables for Threads
-sem_t mutex, toR, toL, print;
+sem_t mutex, toB, toA, print;
 int xingCount = 0;
 int xedCount = 0;
-int toRWaitCount = 0;
-int toLWaitCount = 0;
-int ids[LEFT_NUM + RIGHT_NUM];
+int toAWaitCount = 0;
+int toBWaitCount = 0;
+int ids[A_NUM + B_NUM];
 
 
-typedef enum {None, LtoR, RtoL} dir_t;
+typedef enum {None, AtoB, BtoA} dir_t;
 dir_t xingDirection = None;
 
 
@@ -56,122 +53,122 @@ void printThreadInfo(char* status, char* type, void* id)
 }
 
 
-void* leftCross(void* pthread_id)
+void* a_to_b_cross(void* pthread_id)
 {
-    printThreadInfo("About to sleep before starting", "Left to Right", pthread_id);
+    printThreadInfo("About to sleep before starting", "A -> B", pthread_id);
     thread_sleep(1000);
     
     sem_wait(&mutex);
-    if((xingDirection == LtoR || xingDirection == None) && xingCount < 5 && (xedCount + xingCount) < 10) {
-	xingDirection = LtoR;
+    if((xingDirection == AtoB || xingDirection == None) && xingCount < 5 && (xedCount + xingCount) < 10) {
+	xingDirection = AtoB;
 	xingCount++;
 	sem_post(&mutex);
     } else {
-	toRWaitCount++;
+	toBWaitCount++;
 	sem_post(&mutex);
-	sem_wait(&toR);
-	toRWaitCount--;
+	sem_wait(&toB);
+	toBWaitCount--;
 	xingCount++;
-	xingDirection = LtoR;
+	xingDirection = AtoB;
 	sem_post(&mutex);
     }
 
-    printThreadInfo("Crossing", "Left to Right", pthread_id);
-    thread_sleep(10000);
-    printThreadInfo("Crossed!", "Left to Right", pthread_id);
+    printThreadInfo("Crossing", "A -> B", pthread_id);
+    thread_sleep(1000);
+    printThreadInfo("Crossed!", "A -> B", pthread_id);
     
     sem_wait(&mutex);
     xedCount++;
     xingCount--;
-    if(xingCount > 0 && xingCount >= 4 &&
-       (((xedCount+xingCount) < 10 && toRWaitCount == 0) ||
-	((xedCount+xingCount) >= 10 && toLWaitCount != 0))) {
+    if(xingCount > 0 && xingCount <= 4 &&
+       (((xedCount+xingCount) < 10 && toBWaitCount == 0) ||
+	((xedCount+xingCount) >= 10 && toAWaitCount != 0))) {
 
 	sem_post(&mutex);
 	
-    } else if(xingCount > 0 && xingCount >= 4 &&
-	      (((xedCount+xingCount) < 10 && toRWaitCount !=0) ||
-	       ((xedCount+xingCount) >= 10 && toLWaitCount != 0))) {
+    } else if(xingCount > 0 && xingCount <= 4 &&
+	      (((xedCount+xingCount) < 10 && toBWaitCount !=0) ||
+	       ((xedCount+xingCount) >= 10 && toAWaitCount == 0 && toBWaitCount != 0))) {
 
-	sem_post(&toR);
+	sem_post(&toB);
 
     } else if((xingCount == 0 && (((xedCount+xingCount) < 10 &&
-				   toRWaitCount == 0 && toLWaitCount != 0) ||
-				  ((xedCount+xingCount) >= 10 && toLWaitCount != 0)))) {
+				   toBWaitCount == 0 && toAWaitCount != 0) ||
+				  ((xedCount+xingCount) >= 10 && toAWaitCount != 0)))) {
 
-	xingDirection = RtoL;
+	xingDirection = BtoA;
 	xedCount = 0;
-	sem_post(&toL);
+	sem_post(&toA);
 
-    } else if(xingCount == 0 && toRWaitCount == 0 && toLWaitCount == 0) {
+    } else if(xingCount == 0 && toBWaitCount == 0 && toAWaitCount == 0) {
 
 	xingDirection = None;
 	xedCount = 0;
 	sem_post(&mutex);
 
     } else {
-	printThreadInfo("Condition Error", "Left to Right", pthread_id);
+	printThreadInfo("Condition Error", "A -> B", pthread_id);
 	exit(EXIT_FAILURE);
     }
     return NULL;
 }
 
 
-void* rightCross(void* pthread_id)
+void* b_to_a_cross(void* pthread_id)
 {
-    printThreadInfo("About to sleep before starting", "Right to Left", pthread_id);
+    printThreadInfo("About to sleep before starting", "B -> A", pthread_id);
     thread_sleep(1000);
     
     sem_wait(&mutex);
-    if((xingDirection == RtoL || xingDirection == None) && xingCount < 5 && (xedCount + xingCount) < 10) {
-	xingDirection = RtoL;
+    if((xingDirection == BtoA || xingDirection == None) && xingCount < 5 && (xedCount + xingCount) < 10) {
+	xingDirection = BtoA;
 	xingCount++;
 	sem_post(&mutex);
     } else {
-	toRWaitCount++;
+	toAWaitCount++;
 	sem_post(&mutex);
-	sem_wait(&toL);
-	toRWaitCount--;
+	sem_wait(&toA);
+	toAWaitCount--;
 	xingCount++;
-	xingDirection = RtoL;
+	xingDirection = BtoA;
 	sem_post(&mutex);
     }
 
-    printThreadInfo("Crossing", "Right to Left", pthread_id);
-    thread_sleep(10000);
-    printThreadInfo("Crossed!", "Right to Left", pthread_id);
+    printThreadInfo("Crossing", "B -> A", pthread_id);
+    thread_sleep(1000);
+    printThreadInfo("Crossed!", "B -> A", pthread_id);
     
     sem_wait(&mutex);
     xedCount++;
     xingCount--;
-    if(xingCount > 0 && xingCount >= 4 &&
-       (((xedCount+xingCount) < 10 && toLWaitCount == 0) ||
-	((xedCount+xingCount) >= 10 && toRWaitCount != 0))) {
+    if(xingCount > 0 && xingCount <= 4 &&
+       (((xedCount+xingCount) < 10 && toAWaitCount == 0) ||
+	((xedCount+xingCount) >= 10 && toBWaitCount != 0))) {
 
 	sem_post(&mutex);
 	
-    } else if(xingCount > 0 && xingCount >= 4 &&
-	      (((xedCount+xingCount) < 10 && toLWaitCount !=0) ||
-	       ((xedCount+xingCount) >= 10 && toRWaitCount != 0))) {
+    } else if(xingCount > 0 && xingCount <= 4 &&
+	      (((xedCount+xingCount) < 10 && toAWaitCount !=0) ||
+	       ((xedCount+xingCount) >= 10 && toBWaitCount == 0 && toAWaitCount != 0))) {
 
-	sem_post(&toL);
+	sem_post(&toA);
 
     } else if((xingCount == 0 && (((xedCount+xingCount) < 10 &&
-				   toLWaitCount == 0 && toRWaitCount != 0) ||
-				  ((xedCount+xingCount) >= 10 && toRWaitCount != 0)))) {
+				   toAWaitCount == 0 && toBWaitCount != 0) ||
+				  ((xedCount+xingCount) >= 10 && toBWaitCount != 0)))) {
 
-	xingDirection = LtoR;
+	xingDirection = AtoB;
 	xedCount = 0;
-	sem_post(&toR);
+	sem_post(&toB);
 
-    } else if(xingCount == 0 && toLWaitCount == 0 && toRWaitCount == 0) {
+    } else if(xingCount == 0 && toAWaitCount == 0 && toBWaitCount == 0) {
 
 	xingDirection = None;
 	xedCount = 0;
 	sem_post(&mutex);
 
     } else {
-	printThreadInfo("Condition Error", "Right to Left", pthread_id);
+	printThreadInfo("Condition Error", "B -> A", pthread_id);
 	exit(EXIT_FAILURE);
     }
 
@@ -181,11 +178,11 @@ void* rightCross(void* pthread_id)
 
 int main(int argc, char** argv)
 {
-    pthread_t leftBaboons[LEFT_NUM];
-    pthread_t rightBaboons[RIGHT_NUM];
+    pthread_t aBaboons[A_NUM];
+    pthread_t bBaboons[B_NUM];
     pthread_attr_t attr;
 
-    srandom(RAND_SEED);
+    srandom(time(NULL));
     
     // Explicitly allow the threads to be joinable
     pthread_attr_init(&attr);
@@ -193,42 +190,42 @@ int main(int argc, char** argv)
     
     // Initialize the semaphores
     sem_init(&mutex, THREAD_SHARED, 1);
-    sem_init(&toL, THREAD_SHARED, 0);
-    sem_init(&toR, THREAD_SHARED, 0);
+    sem_init(&toA, THREAD_SHARED, 0);
+    sem_init(&toB, THREAD_SHARED, 0);
     sem_init(&print, THREAD_SHARED, 1);
 
-    // Create Left Crossing Baboons
-    for(int i = 0; i < LEFT_NUM; i++) {
+    // Create A->B Crossing Baboons
+    for(int i = 0; i < A_NUM; i++) {
 	ids[i] = i;
-	if(pthread_create(&leftBaboons[i], &attr, leftCross, (void*)&ids[i]) == -1) {
-	    perror("Thread Creation Error: Left");
+	if(pthread_create(&aBaboons[i], &attr, a_to_b_cross, (void*)&ids[i]) == -1) {
+	    perror("Thread Creation Error: A");
 	    exit(EXIT_FAILURE);
 	}
     }
 
-    // Create Right Crossing Baboons
-    for(int i = 0; i < RIGHT_NUM; i++) {
-	ids[LEFT_NUM+i] = i;
-	if(pthread_create(&rightBaboons[i], &attr, rightCross, (void*)&ids[LEFT_NUM+i]) == -1) {
-	    perror("Thread Creation Error: Right");
+    // Create B->A Crossing Baboons
+    for(int i = 0; i < B_NUM; i++) {
+	ids[A_NUM+i] = i;
+	if(pthread_create(&bBaboons[i], &attr, b_to_a_cross, (void*)&ids[A_NUM+i]) == -1) {
+	    perror("Thread Creation Error: B");
 	    exit(EXIT_FAILURE);
 	}
     }
 
     void* status;
 
-    // Join Left Crossing Baboons
-    for(int i = 0; i < LEFT_NUM; i++) {
-	if(pthread_join(leftBaboons[i], &status) == -1) {
-	    perror("Thread Join Error: Left");
+    // Join A Crossing Baboons
+    for(int i = 0; i < A_NUM; i++) {
+	if(pthread_join(aBaboons[i], &status) == -1) {
+	    perror("Thread Join Error: A");
 	    exit(EXIT_FAILURE);
 	}
     }
 
-    // Join Right Crossing Baboons
-    for(int i = 0; i < RIGHT_NUM; i++) {
-	if(pthread_join(rightBaboons[i], &status) == -1) {
-	    perror("Thread Join Error: Right");
+    // Join B Crossing Baboons
+    for(int i = 0; i < B_NUM; i++) {
+	if(pthread_join(bBaboons[i], &status) == -1) {
+	    perror("Thread Join Error: B");
 	    exit(EXIT_FAILURE);
 	}
     }
